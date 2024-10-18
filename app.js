993e048a206bd5ffb29f55c55803c3925407748b
JoaoRSS <joaors987654321@gmail.com>
2024-10-18 16:52:28 -0300

    upado commit

---
+++ app.js
@@ -0,0 +1,91 @@
+
+import { galeria } from '/galeria.js';
+
+window.galeria1 = new galeria("recipins", "recipins2", "recipins3", 1);
+
+window.galeria2 = new galeria("PHPMysqli", "PHPMysqli2", "PHPMysqli3", 1);
+
+window.galeria3 = new galeria("decodificador", "decodificador2", "decodificador3", 1);
+
+
+document.addEventListener("scroll", scrollIF);
+
+function scrollIF() {
+
+    const alvo = (document.getElementById("form1").getBoundingClientRect().top);
+
+    const janela = window.innerHeight;
+    
+    if (janela >= alvo) {
+        document.getElementById("formacao1").animate(
+        [
+            { transform: "translateX(-200px)", opacity: "0"},
+            { transform: "translateX(0px)", opacity: "100"},
+        ],
+        {
+            // timing options
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-out"
+
+        },
+
+        );
+        document.removeEventListener("scroll", scrollIF)
+    }
+}
+
+
+document.addEventListener("scroll", scrollONE);
+
+function scrollONE() {
+
+    const alvo = (document.getElementById("form2").getBoundingClientRect().top);
+
+    const janela = window.innerHeight;
+    
+    if (janela >= alvo) {
+        document.getElementById("formacao2").animate(
+        [
+            { transform: "translateX(200px)", opacity: "0"},
+            { transform: "translateX(0px)", opacity: "100"},
+        ],
+        {
+            // timing options
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-out"
+
+        },
+
+        );
+        document.removeEventListener("scroll", scrollONE)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadSkills)
+
+let i = 1;
+
+function loadSkills(){
+
+     if (i < 9){
+        let skillvez = `skill${i}`
+        console.log(skillvez);
+        document.getElementById(skillvez).animate(
+            [
+                { transform: "translateY(-30px)", opacity: "0"},
+                { transform: "translateY(0px)", opacity: "100"},
+            ],
+            {
+                // timing options
+                duration: 600,
+                fill: "forwards",
+                easing: "ease-out"
+            },
+        );
+
+        i = i+1;
+        setTimeout(loadSkills, 50);
+    }
+}
---
+++ galeria.js
@@ -0,0 +1,73 @@
+export class galeria {
+    constructor(img1, img2, img3, n) {
+            this.img1 = img1;
+            this.img2 = img2;
+            this.img3 = img3;
+            this.n = n;
+    }
+
+    
+    proximo(){
+
+        console.log(this.n)
+
+        if (this.n == 1){
+
+            document.getElementById(this.img1).setAttribute("src", "images/"+this.img2+".png");
+            document.getElementById(this.img2).setAttribute("src", "images/"+this.img3+".png"); 
+            document.getElementById(this.img3).setAttribute("src", "images/"+this.img1+".png");
+    
+            this.n = this.n + 1;
+            
+        } else if (this.n == 2){
+            
+            document.getElementById(this.img1).setAttribute("src", "images/"+this.img3+".png");
+            document.getElementById(this.img2).setAttribute("src", "images/"+this.img1+".png"); 
+            document.getElementById(this.img3).setAttribute("src", "images/"+this.img2+".png");
+    
+            this.n = this.n + 1;
+            
+        } else if (this.n == 3){
+    
+            document.getElementById(this.img1).setAttribute("src", "images/"+this.img1+".png");
+            document.getElementById(this.img2).setAttribute("src", "images/"+this.img2+".png");     
+            document.getElementById(this.img3).setAttribute("src", "images/"+this.img3+".png");
+    
+            this.n = 1;
+
+        }
+    }
+    
+    anterior(){
+        
+        console.log(this.n);
+
+
+        if (this.n == 3 || this.n > 3){
+
+            document.getElementById(this.img1).setAttribute("src", "images/"+this.img2+".png");
+            document.getElementById(this.img2).setAttribute("src", "images/"+this.img3+".png"); 
+            document.getElementById(this.img3).setAttribute("src", "images/"+this.img1+".png");
+    
+            this.n = this.n - 1;
+            
+        } else if (this.n == 2){
+            
+            document.getElementById(this.img1).setAttribute("src", "images/"+this.img3+".png");
+            document.getElementById(this.img2).setAttribute("src", "images/"+this.img1+".png"); 
+            document.getElementById(this.img3).setAttribute("src", "images/"+this.img2+".png");
+    
+            this.n = this.n - 1;
+            
+        } else if (this.n == 1 || this.n < 1){
+    
+            document.getElementById(this.img1).setAttribute("src", "images/"+this.img1+".png");
+            document.getElementById(this.img2).setAttribute("src", "images/"+this.img2+".png");     
+            document.getElementById(this.img3).setAttribute("src", "images/"+this.img3+".png");
+    
+            this.n = 3;
+        
+        }
+    }
+
+}
